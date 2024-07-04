d4fa4789a56de4bb102897628b0bd447061a297c
Mary <marisol.pereira.goncalves@gmail.com>
2024-07-05 01:39:10 +0200

    Practica Final

--- angular-17-app/src/app/app.component.ts
+++ angular-17-app/src/app/app.component.ts
@@ -2,16 +2,17 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserComponent } from './user/user.component';
+import { CommentsComponent } from "./comments/comments.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet,
-            CommonModule,
-            UserComponent
-  ],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet,
+        CommonModule,
+        UserComponent, 
+        CommentsComponent]
 })
 export class AppComponent {
   title = 'angular-17-app';
