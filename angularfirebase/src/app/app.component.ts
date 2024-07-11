6497011f55ece9b283eba19b278716123d51bfc0
Mary <marisol.pereira.goncalves@gmail.com>
2024-07-11 19:18:29 +0200

    CambioFirebaserc+js+ts+ environments--prod---ts

--- angularfirebase/src/app/app.component.ts
+++ angularfirebase/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { environment } from '../environments/environments'; // crear una ruta
 
 @Component({
   selector: 'app-root',
@@ -9,5 +10,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'angularfirebase';
+  title = environment.title;
 }
---
+++ angularfirebase/src/environments/environments.prod.ts
@@ -0,0 +1,13 @@
+export const environment = {
+    production: true,
+    firebaseConfig: {
+        apiKey: "AIzaSyA7jS6IEJvwNzBm13Hd225EHxxhUHj51ME",
+        authDomain: "fir-angularprod-1ae3f.firebaseapp.com",
+        projectId: "fir-angularprod-1ae3f",
+        storageBucket: "fir-angularprod-1ae3f.appspot.com",
+        messagingSenderId: "1024121374532",
+        appId: "1:1024121374532:web:3ce996222ac27cd55d7911",
+        measurementId: "G-B7ES7D8H5B"
+    },
+    title: 'AngularFirebaseProyect!! PROD' // entorno de produccion
+  };
