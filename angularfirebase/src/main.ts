0574994115dba03f419c61afa1088b5064a96e85
Mary <marisol.pereira.goncalves@gmail.com>
2024-07-16 21:26:22 +0200

    AngularFirebaseBasic

---
+++ angularfirebase/src/main.ts
@@ -0,0 +1,16 @@
+import { bootstrapApplication } from '@angular/platform-browser';
+import { provideRouter } from '@angular/router';
+import { AppComponent } from './app/app.component';
+import { routes } from './app/app.routes';
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { environment } from './environments/environments';
+
+bootstrapApplication(AppComponent, {
+  providers: [
+    provideRouter(routes),
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideAuth(() => getAuth())
+  ]
+}).catch(err => console.error(err));
+
