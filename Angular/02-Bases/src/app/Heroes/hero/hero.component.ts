f449c2eebf2c7be0e44361829ecbab3ad653ad11
Mary <marisol.pereira.goncalves@gmail.com>
2024-08-30 12:29:13 +0200

    cambia-Edad-Nombre

--- Angular/02-Bases/src/app/Heroes/hero/hero.component.ts
+++ Angular/02-Bases/src/app/Heroes/hero/hero.component.ts
@@ -8,4 +8,22 @@ import { Component } from '@angular/core';
 export class HeroComponent {
   public name: string = 'ironman';
   public age: number = 30;
+
+  get capitalizedName(): string {
+    return  this.name.toUpperCase();
+  }
+
+  //Metodo
+  getHeroDescription(): string {
+    return `${this.name} - ${this.age}`;
+  }
+
+  changeHero(): void {
+    // throw 'Metodo por realizar';
+    this.name = 'Spiderman';
+  }
+
+  changeAge(): void {
+    this.age =25;
+  }
 }
