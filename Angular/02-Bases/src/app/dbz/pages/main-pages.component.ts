080b60fcda42afef307a26d8d267e0ca3a8e01f5
Mary <marisol.pereira.goncalves@gmail.com>
2024-09-03 14:36:08 +0200

    services + npm uuid

--- Angular/02-Bases/src/app/dbz/pages/main-pages.component.ts
+++ Angular/02-Bases/src/app/dbz/pages/main-pages.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
+import { DbzService } from '../services/dbz.service';
 
 @Component({
   selector: 'app-dbz-main-page',
@@ -7,25 +8,5 @@ import { Character } from '../interfaces/character.interface';
   styleUrl: 'main-pages.component.css',
 })
 export class MainPageComponent {
-
-  public characters: Character[]= [{
-    name:'Krillin',
-    power: 1000
-  },{
-    name:'Goku',
-    power: 9500
-  },{
-    name:'Vegeta',
-    power: 7500,
-  }];
-
-  onNewCharacter(Character: Character): void {
-    // console.error('MainPage');
-    // console.warn(Character);
-    this.characters.push(Character);
-  }
-
-  onDeleteCharacter(index: number): void {
-    this.characters.splice(index,1);
-  }
+  constructor(public DbzService: DbzService){};
 }
