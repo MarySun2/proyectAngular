3fac31ab7294d27db694f5b07800dda35df2e0f9
Mary <marisol.pereira.goncalves@gmail.com>
2024-09-03 15:08:22 +0200

    paquete externo npm uuid identificador unico

--- Angular/02-Bases/src/app/dbz/services/dbz.service.ts
+++ Angular/02-Bases/src/app/dbz/services/dbz.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { Character } from "../interfaces/character.interface";
+import { v4 as uuid} from "uuid";
+import { Character } from '../interfaces/character.interface';
 
 
 @Injectable({
@@ -8,24 +9,36 @@ import { Character } from "../interfaces/character.interface";
 export class DbzService {
 
     public characters: Character[]= [{
+    id: uuid(),
     name:'Krillin',
     power: 1000
   },{
+    id: uuid(),
     name:'Goku',
     power: 9500
   },{
+    id: uuid(),
     name:'Vegeta',
     power: 7500,
   }];
 
-  onNewCharacter(Character: Character): void {
+  onNewCharacter(character: Character): void {
     // console.error('MainPage');
     // console.warn(Character);
-    this.characters.push(Character);
+    //this.characters.push(Character);
+    const newCharacter: Character = {id: uuid(), ...character} // para todo los characteres name y power incluye id
+
+    this.characters.push(newCharacter);
   }
 
-  onDeleteCharacter(index: number): void {
-    this.characters.splice(index,1);
+  //borra sin un id unico
+  // onDeleteCharacter(index: number): void {
+  //   this.characters.splice(index,1);
+  // }
+
+  // Borra con id unico
+  deleteCharacter(id:string) {
+    this.characters = this.characters.filter (character => character.id !== id);
   }
 
 }
