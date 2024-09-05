4b6bc9a73c4b303bec330584fed1f2782862fd9b
Mary <marisol.pereira.goncalves@gmail.com>
2024-09-05 15:38:24 +0200

    peticion http

--- Angular/03-gifs-app/src/app/app.module.ts
+++ Angular/03-gifs-app/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
-import { HomePageComponent } from './gifs/pages/home/home-page.component';
+
+
 
 @NgModule({
   declarations: [
@@ -14,8 +17,9 @@ import { HomePageComponent } from './gifs/pages/home/home-page.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
+    HttpClientModule,
     GifsModule,
-    SharedModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- Angular/03-gifs-app/src/app/gifs/services/gifs.service.ts
+++ Angular/03-gifs-app/src/app/gifs/services/gifs.service.ts
@@ -1,4 +1,8 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+
+//Otra forma de hacerlo
+//const GIPHY_API_KEY = 'beBWg9oYZvJ5MiZFRROV7RASF3Jg49kw';
 
 @Injectable({
   providedIn: 'root'
@@ -6,8 +10,10 @@ import { Injectable } from '@angular/core';
 export class GifsService {
 
   private _tagsHistory:string[] = [];
+  private apiKey:       string ='beBWg9oYZvJ5MiZFRROV7RASF3Jg49kw';
+  private serviceUrl:   string ='https://api.giphy.com/v1/gifs';
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   get tagsHistory() {
     return [...this._tagsHistory];
@@ -27,14 +33,35 @@ export class GifsService {
     if(tag.length ===0) return;
 
      // Validación para evitar números
-     const regex = /^[a-zA-Z]+$/;
-     if (!regex.test(tag)) {
-       console.log('No se permiten números en el tag.');
-       return; // Sale del método si el tag contiene números
-     }
+    //  const regex = /^[a-zA-Z]+$/;
+    //  if (!regex.test(tag)) {
+    //    console.log('No se permiten números en el tag.');
+    //    return; // Sale del método si el tag contiene números
+    //  }
 
   this.organizeHistory(tag);
 
   console.log(this._tagsHistory);
+
+  const params = new HttpParams()
+  .set('api_key', this.apiKey)
+  .set('limit', 10)
+  .set('q', tag)
+
+  this.http.get(`${this.serviceUrl}/search`, {params}) // Se puede reducir
+  .subscribe(resp => {
+    console.log(resp);
+  });
+
+
+  // Primera Forma de hacerlo:
+  // fetch('https://api.giphy.com/v1/gifs/search?api_key=beBWg9oYZvJ5MiZFRROV7RASF3Jg49kw&q=valorant&limit=10')
+  // .then(resp => resp.json())
+  // .then(data=> console.log(data));
+
+  //Segunda Forma de hacerlo
+  // const resp = await fetch('https://api.giphy.com/v1/gifs/search?api_key=beBWg9oYZvJ5MiZFRROV7RASF3Jg49kw&q=valorant&limit=10')
+  // const data = await resp.json();
+  // console.log(data);
   }
 }
