dc55932f3002ffa17d8c9f22496d92a8b4f58cd4
Mary <marisol.pereira.goncalves@gmail.com>
2024-09-30 16:11:51 +0200

    Countries Service(apprestcountries)

--- Angular/04-countryApp/src/app/app.module.ts
+++ Angular/04-countryApp/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
 import { AppRoutingModule } from './app.routing.module';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -11,7 +12,8 @@ import { AppRoutingModule } from './app.routing.module';
   imports: [
     BrowserModule,
     SharedModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
