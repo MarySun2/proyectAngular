640683e20f6bb20621452ce3245565fc017710b1
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-01 22:46:31 +0200

    region statica

--- Angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ Angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { CountriesService } from '../../services/countries.service';
+import { Country } from '../../interfaces/country';
 
 @Component({
   selector: 'countries-by-region-page',
@@ -6,5 +8,16 @@ import { Component } from '@angular/core';
   styles: ``
 })
 export class ByRegionPageComponent {
+//Creo una propiedad
+public countries: Country[] = [];
 
+// Se Inyecta el servicio
+constructor(private countriesService: CountriesService) {}
+
+searchRegion(term: string): void {
+  this.countriesService.searchRegion(term)
+  .subscribe(countries => {
+    this.countries = countries;
+  });
+}
 }
