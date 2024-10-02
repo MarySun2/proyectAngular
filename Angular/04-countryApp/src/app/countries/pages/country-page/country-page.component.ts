8e337d759e47609b5de1e807d4c9548a01cc0ee3
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-02 19:28:10 +0200

    switchMap- Refactorizacion

--- Angular/04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
+++ Angular/04-countryApp/src/app/countries/pages/country-page/country-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { CountriesService } from '../../services/countries.service';
+import { switchMap } from 'rxjs';
 
 
 @Component({
@@ -14,19 +15,52 @@ export class CountryPageComponent implements OnInit  {
   constructor(
     //injeccion
     private activatedRoute: ActivatedRoute,
+    private router: Router,
     private countriesService: CountriesService,
   ) {}
 
-  // implementacion del OnInit
+  //1. forma de hacerlo
+  // ngOnInit(): void {
+  //   this.activatedRoute.params
+  //   // .subscribe((params: any) => {
+  //     .subscribe(({id}) => {
+
+  //     this.countriesService.searchCountryByAlphaCode( id )
+  //     .subscribe( country => {
+  //       console.log( {country})
+  //     });
+  //   });
+  // }
+  // 2. forma de hacerlo
+  // implementacion del OnInit y un observable held
+  // ngOnInit(): void {
+  //   this.activatedRoute.params
+  //   // .subscribe((params: any) => {
+  //     .subscribe(({id}) => {
+
+  //     this.countriesService.searchCountryByAlphaCode( id )
+  //     .subscribe( country => {
+  //       console.log( {country})
+  //     });
+  //   });
+  // }
+
+  // 3. forma de hacerlo
   ngOnInit(): void {
     this.activatedRoute.params
-    // .subscribe((params: any) => {
-      .subscribe(({id}) => {
 
-      this.countriesService.searchCountryByAlphaCode( id )
+    .pipe(
+      switchMap (({id}) => this.countriesService.searchCountryByAlphaCode( id )),
+    )
+
       .subscribe( country => {
-        console.log( {country})
+
+        if ( !country ) {
+          return this.router.navigateByUrl('');
+        }
+        console.log('TENEMOS UN PAIS');
+        return;
       });
-    });
+
   }
 }
--- Angular/04-countryApp/src/app/countries/services/countries.service.ts
+++ Angular/04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, Observable, of } from 'rxjs';
+import { catchError, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
 @Injectable({
@@ -13,13 +13,14 @@ export class CountriesService {
   constructor( private http: HttpClient ) {}
 
   //Metodo de busqueda
-  searchCountryByAlphaCode( code: string): Observable<Country[]> {
+  searchCountryByAlphaCode( code: string): Observable<Country | null> {
 
     const url = `${ this.apiUrl }/alpha/${ code }`;
 
       return this.http.get<Country[]>(url)
       .pipe(
-        catchError( error => of ([]) )
+        map( countries => countries.length > 0 ? countries[0]: null),
+        catchError( error => of (null) )
       );
   }
 
