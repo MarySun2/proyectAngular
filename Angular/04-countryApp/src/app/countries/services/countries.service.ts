b80ebb2f062e1025594079f9a4c2998f9b65aee6
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-03 15:11:51 +0200

    Refactorizando

--- Angular/04-countryApp/src/app/countries/services/countries.service.ts
+++ Angular/04-countryApp/src/app/countries/services/countries.service.ts
@@ -1,54 +1,60 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError, map, Observable, of } from 'rxjs';
+import { catchError, delay, map, Observable, of } from 'rxjs';
 import { Country } from '../interfaces/country';
 
+// Decorador Injectable que permite la inyección de este servicio en otros componentes o servicios
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root' // Este servicio estará disponible en toda la aplicación
 })
 export class CountriesService {
 
+  // URL base de la API de países
   private apiUrl: string = 'https://restcountries.com/v3.1';
 
-  constructor( private http: HttpClient ) {}
+  // Inyección del servicio HttpClient para realizar solicitudes HTTP
+  constructor(private http: HttpClient) {}
 
-  //Metodo de busqueda
-  searchCountryByAlphaCode( code: string): Observable<Country | null> {
+  private getCountriesRequest( url: string): Observable<Country[]>{
+    return this.http.get<Country[]>(url)
+    .pipe(
+      catchError(error => of([])), // Captura cualquier error y devuelve null
+      delay(2000), // Retraza el tiempo de la consulta
+    );
+  }
 
-    const url = `${ this.apiUrl }/alpha/${ code }`;
+  // Método para buscar un país por su código alfa
+  searchCountryByAlphaCode(code: string): Observable<Country | null> {
 
-      return this.http.get<Country[]>(url)
+    const url = `${this.apiUrl}/alpha/${code}`; // Construye la URL para la solicitud
+
+    return this.http.get<Country[]>(url) // Realiza la solicitud GET
       .pipe(
-        map( countries => countries.length > 0 ? countries[0]: null),
-        catchError( error => of (null) )
+        map(countries => countries.length > 0 ? countries[0] : null), // Mapea la respuesta para obtener el primer país o null
+        catchError(error => of(null)) // Captura cualquier error y devuelve null
       );
   }
 
-    //Metodo para Capitales
-    searchCapital( term: string ): Observable<Country[]> {
+  // Método para buscar países por capital
+  searchCapital(term: string): Observable<Country[]> {
 
-      const url = `${ this.apiUrl }/capital/${ term }`;
-
-      return this.http.get<Country[]>(url)
-      .pipe(
-        catchError( error => of ([]) )
-      );
-    }
-    //Paises
-    searchCountry( term: string ): Observable<Country[]> {
-      const url = `${ this.apiUrl }/name/${ term }`;
-      return this.http.get<Country[]>(url)
-      .pipe(
-        catchError( error => of ([]) )
-      );
-    }
-    //Region
-    searchRegion( region: string ): Observable<Country[]> {
-      const url = `${ this.apiUrl }/region/${ region }`;
-      return this.http.get<Country[]>(url)
-      .pipe(
-        catchError( error => of ([]) )
-      );
-    }
+    const url = `${this.apiUrl}/capital/${term}`; // Construye la URL para la solicitud
+    return this.getCountriesRequest(url);
   }
+
+  // Método para buscar países por nombre
+  searchCountry(term: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/name/${term}`; // Construye la URL para la solicitud
+    return this.getCountriesRequest(url);
+  }
+
+  // Método para buscar países por región
+  searchRegion(region: string): Observable<Country[]> {
+
+    const url = `${this.apiUrl}/region/${region}`; // Construye la URL para la solicitud
+    return this.getCountriesRequest(url);
+  }
+}
+
 
