190b168f8aa0aedcd10324bf8ddb2e9e020a8e4a
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-04 17:27:54 +0200

    byRegion

--- Angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
+++ Angular/04-countryApp/src/app/countries/pages/by-region-page/by-region-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { CountriesService } from '../../services/countries.service';
 import { Country } from '../../interfaces/country';
 
+type Region = 'Africa' | 'America'| 'Asia'|'Europe'| 'Oceania'
+
 @Component({
   selector: 'countries-by-region-page',
   templateUrl: './by-region-page.component.html',
@@ -10,12 +12,14 @@ import { Country } from '../../interfaces/country';
 export class ByRegionPageComponent {
 //Creo una propiedad
 public countries: Country[] = [];
+public regions: Region[] = ['Africa', 'America', 'Asia','Europe', 'Oceania'];
+public selectedRegion?: Region;
 public isLoading: boolean = false;
 
 // Se Inyecta el servicio
 constructor(private countriesService: CountriesService) {}
 
-searchRegion(region: string): void {
+searchRegion(region: Region): void {
 
   this.isLoading = true;
 
