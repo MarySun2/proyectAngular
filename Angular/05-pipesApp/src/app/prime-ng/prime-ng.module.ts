3bafffcdb94d549a874c7edd88264979c5c36684
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-07 21:14:54 +0200

    confi animatin Menus

---
+++ Angular/05-pipesApp/src/app/prime-ng/prime-ng.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { MenuModule } from 'primeng/menu';
+import { ButtonModule } from 'primeng/button';
+
+
+
+@NgModule({
+  declarations: [],
+  exports: [
+    MenuModule,
+    ButtonModule
+  ]
+})
+export class PrimeNgModule { }
---
+++ Angular/05-pipesApp/src/app/shared/components/menu/menu.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+
+@Component({
+  selector: 'shared-menu',
+  templateUrl: './menu.component.html',
+  styles: ``
+})
+export class MenuComponent implements OnInit {
+
+  public menuItems: MenuItem[] = [];
+  public items: MenuItem[] = [];
+
+  ngOnInit() {
+    this.menuItems = [
+      { label: 'New', icon: 'pi pi-fw pi-plus' },
+      { label: 'Open', icon: 'pi pi-fw pi-download' },
+      { label: 'Undo', icon: 'pi pi-fw pi-refresh' }
+    ];
+
+
+    this.items = [
+      {
+        label: 'Options',
+        items: [
+          {
+            label: 'Refresh',
+            icon: 'pi pi-refresh'
+          },
+          {
+            label: 'Export',
+            icon: 'pi pi-upload'
+          }
+        ]
+      }
+    ];
+  }
+}
+
