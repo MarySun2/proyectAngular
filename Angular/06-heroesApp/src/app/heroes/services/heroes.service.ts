57c4f3e00e45c389ef0591899ae1bea95da9bd82
Mary <marisol.pereira.goncalves@gmail.com>
2024-10-16 23:19:18 +0200

    heroService (Traer informacion de los heroes)

---
+++ Angular/06-heroesApp/src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,23 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environments } from '../../../environments/environments';
+import { Hero } from '../interfaces/hero.interface';
+import { Observable } from 'rxjs';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  private baseUrl: string = environments.baseUrl;
+
+  constructor(private httpClient: HttpClient) { }
+
+  getHeroes() :Observable<Hero[]>{
+
+    return this.httpClient.get<Hero[]>(`${ this.baseUrl}/heroes`);
+  }
+
+
+}
