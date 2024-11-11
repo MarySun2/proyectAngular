5ea55096265328d3ee775dd118f871903e964f7b
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-11 23:37:55 +0100

    Eliminar Registros

--- Angular/06-heroesApp/src/app/heroes/heroes.module.ts
+++ Angular/06-heroesApp/src/app/heroes/heroes.module.ts
@@ -11,6 +11,7 @@ import { SearchPageComponent } from './pages/search-page/search-page.component';
 import { MaterialModule } from '../material/material.module';
 import { CardComponent } from './components/card/card.component';
 import { HeroImagePipe } from './pipes/hero-image.pipe';
+import { ConfirmDialogComponent } from './components/confirm-dialog/confirm-dialog.component';
 
 
 
@@ -23,7 +24,8 @@ import { HeroImagePipe } from './pipes/hero-image.pipe';
     SearchPageComponent,
     CardComponent,
     //Pipe
-    HeroImagePipe
+    HeroImagePipe,
+    ConfirmDialogComponent
   ],
   imports: [
     CommonModule,
--- Angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ Angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -3,8 +3,11 @@ import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { switchMap } from 'rxjs';
+import { filter, switchMap, tap } from 'rxjs';
+
 import { MatSnackBar } from '@angular/material/snack-bar';
+import { MatDialog } from '@angular/material/dialog';
+import { ConfirmDialogComponent } from '../../components/confirm-dialog/confirm-dialog.component';
 
 @Component({
   selector: 'app-new-page',
@@ -35,6 +38,7 @@ export class NewPageComponent implements OnInit {
     private activatedRoute: ActivatedRoute,
     private router: Router,
     private snackbar: MatSnackBar,
+    private dialog: MatDialog,
   ) {}
 
   get currentHero() : Hero {
@@ -81,6 +85,39 @@ export class NewPageComponent implements OnInit {
     .subscribe(hero =>{
       this.router.navigate(['/heroes', hero.id]);
       this.showSnackbar(`$ { hero.superhero } Created!`);
+    });
+  }
+
+  onDeleteHero() {
+    if ( !this.currentHero.id ) throw Error('Hero id is Required');
+
+    const dialogRef = this.dialog.open(ConfirmDialogComponent, {
+      data: this.heroForm.value
+    });
+
+    // dialogRef.afterClosed().subscribe(result => {
+    //   if ( !result ) return;
+
+    //   this.heroesService.deleteHeroById( this.currentHero.id )
+    //   .subscribe( wasDeleted => {
+    //     if ( wasDeleted )
+    //       this.router.navigate(['/heroes']);
+    //   })
+    // });
+
+    //OTRA FORMA DE HACERLO
+    dialogRef.afterClosed()
+    .pipe(
+      filter((result: boolean) => result ),
+      // tap( result => console.log(result) )
+      switchMap( () => this.heroesService.deleteHeroById( this.currentHero.id ) ),
+      // tap( wasDeleted => console.log({wasDeleted})),
+      filter( (wasDeleted: boolean) => wasDeleted ),
+    )
+
+    .subscribe(() => {
+      // console.log({result});
+      this.router.navigate(['/heroes']);
     });
   }
 
