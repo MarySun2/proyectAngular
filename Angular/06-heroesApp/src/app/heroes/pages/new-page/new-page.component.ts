a819118ae3eb854abe467be800e32d77632e383a
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-11 21:23:08 +0100

    establecer el valor del formulario

--- Angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
+++ Angular/06-heroesApp/src/app/heroes/pages/new-page/new-page.component.ts
@@ -1,14 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interfaces/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { HttpParams } from '@angular/common/http';
+import { Subscription, switchMap } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
   templateUrl: './new-page.component.html',
   styles: ``
 })
-export class NewPageComponent {
+export class NewPageComponent implements OnInit {
 
   public heroForm = new FormGroup({
   id:                 new FormControl<string>(''),
@@ -27,11 +30,32 @@ export class NewPageComponent {
   ];
 
   //Constructor
-  constructor(private heroesService : HeroesService) {}
+  constructor(
+    private heroesService : HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+  ) {}
 
   get currentHero() : Hero {
     const hero = this.heroForm.value as Hero;
     return hero;
+  }
+
+  ngOnInit(): void {
+
+    if ( !this.router.url.includes('edit')) return;
+
+    this.activatedRoute.params
+    .pipe(
+      switchMap( ({ id }) => this.heroesService.getHeroById( id ) ),
+    ).subscribe( hero => {
+
+      if ( !hero ) return this.router.navigateByUrl('/');
+
+      this.heroForm.reset( hero );
+      return;
+    });
+
   }
 
   //Metodo
