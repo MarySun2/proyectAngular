90d67628fd1bb58008194eb4933989b27975a33f
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-12 23:24:54 +0100

    implementar funcion guard

--- Angular/06-heroesApp/src/app/auth/guards/auth.guard.service.ts
+++ Angular/06-heroesApp/src/app/auth/guards/auth.guard.service.ts
@@ -1,24 +1,41 @@
 import { Injectable } from '@angular/core';
-import { CanMatch, CanActivate, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
-import { Observable } from 'rxjs';
+import { CanMatch, CanActivate, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthGuard  implements CanMatch, CanActivate{
 
-  constructor() { }
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+
+    return this.authService.checkAuthentication()
+    .pipe(
+      tap( isAuthenticated => console.log('Authenticated:', isAuthenticated ) ),
+      tap (isAuthenticated => {
+        if ( !isAuthenticated ) {
+          this.router.navigate(['./auth/login'])
+        }
+      }),
+    )
+  }
 
   canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
-    console.log('canMatch');
-    console.log({ route, segments });
+    // console.log('canMatch');
+    // console.log({ route, segments });
 
-    return true;
+     return this.checkAuthStatus();
   }
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
-    console.log('canActivate');
-    console.log({ route, state })
+    // console.log('canActivate');
+    // console.log({ route, state })
 
-    return true;
+     return this.checkAuthStatus();
   }
 }
--- Angular/06-heroesApp/src/app/auth/services/auth.service.ts
+++ Angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -1,51 +1,53 @@
-import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+import { Observable, tap, of, map, catchError } from 'rxjs';
+
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { catchError, map, Observable, of, tap } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({providedIn: 'root'})
 export class AuthService {
 
   private baseUrl = environments.baseUrl;
   private user?: User;
 
-  constructor( private http: HttpClient) { }
+  constructor(private http: HttpClient) { }
 
-  get currentUser(): User|undefined {
+  get currentUser():User|undefined {
     if ( !this.user ) return undefined;
-    return structuredClone(this.user);
+    return structuredClone( this.user );
   }
 
-  login( email: string, password: string ): Observable<User> {
-
-    // http.post('login',{ email, password});
-
-    return this.http.get<User>(`${ this.baseUrl}/users/1`)
-    .pipe(
-      tap( user=> this.user = user),
-      tap(user =>localStorage.setItem('token', user.id.toString())),
-    )
+  login( email: string, password: string ):Observable<User> {
+    // http.post('login',{ email, password });
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        tap( user => localStorage.setItem('token', 'aASDgjhasda.asdasd.aadsf123k' )),
+      );
   }
 
-  checkAuthentication(): Observable<boolean> | boolean {
+  checkAuthentication(): Observable<boolean> {
 
-    if ( !localStorage.getItem('token') ) return false;
+    if ( !localStorage.getItem('token') ) return of(false);
 
     const token = localStorage.getItem('token');
 
-    return this.http.get<User>(`${ this.baseUrl} /users/1`)
-    .pipe (
-      tap( user => this.user = user),
-      map( user => !!user ),
-      catchError( err => of (false) )
-    )
+    return this.http.get<User>(`${ this.baseUrl }/users/1`)
+      .pipe(
+        tap( user => this.user = user ),
+        map( user => !!user ),
+        catchError( err => of(false) )
+      );
+
   }
+
 
   logout() {
     this.user = undefined;
     localStorage.clear();
   }
+
+
 }
