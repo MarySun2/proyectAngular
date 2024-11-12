019c4ad4469c3392b249627ab15f32dc664990f3
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-12 21:21:25 +0100

    Autenticacion mostrar usuario Activo

--- Angular/06-heroesApp/src/app/auth/services/auth.service.ts
+++ Angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -29,4 +29,9 @@ export class AuthService {
       tap(user =>localStorage.setItem('token', user.id.toString())),
     )
   }
+
+  logout() {
+    this.user = undefined;
+    localStorage.clear();
+  }
 }
