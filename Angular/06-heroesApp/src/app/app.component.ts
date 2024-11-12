c01e7aac6e1a742a4ef44e68fa2527eeb23a9af2
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-12 21:53:56 +0100

    Mantener Seccion user

--- Angular/06-heroesApp/src/app/app.component.ts
+++ Angular/06-heroesApp/src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent  {
   title = 'heroesApp';
+
+  // constructor(private authService: AuthService) {}
+
+  // ngOnInit(): void {
+  //   this.authService.checkAuthentication().subscribe( () =>{
+  //     console.log('Check Authentication Finished');
+  //   })
+  // }
+
+
 }
--- Angular/06-heroesApp/src/app/auth/services/auth.service.ts
+++ Angular/06-heroesApp/src/app/auth/services/auth.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environments } from '../../../environments/environments';
 import { User } from '../interfaces/user.interface';
-import { Observable, tap } from 'rxjs';
+import { catchError, map, Observable, of, tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -27,6 +27,20 @@ export class AuthService {
     .pipe(
       tap( user=> this.user = user),
       tap(user =>localStorage.setItem('token', user.id.toString())),
+    )
+  }
+
+  checkAuthentication(): Observable<boolean> | boolean {
+
+    if ( !localStorage.getItem('token') ) return false;
+
+    const token = localStorage.getItem('token');
+
+    return this.http.get<User>(`${ this.baseUrl} /users/1`)
+    .pipe (
+      tap( user => this.user = user),
+      map( user => !!user ),
+      catchError( err => of (false) )
     )
   }
 
