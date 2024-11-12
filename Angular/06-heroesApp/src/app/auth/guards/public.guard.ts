b58c4e353732b824813fc0f4ae0692890b4300f8
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-12 23:55:12 +0100

    Guard Login

---
+++ Angular/06-heroesApp/src/app/auth/guards/public.guard.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@angular/core';
+import { CanMatch, CanActivate, Route, UrlSegment, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { map, Observable, tap } from 'rxjs';
+import { AuthService } from '../services/auth.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PublicGuard implements CanActivate, CanMatch {
+
+  constructor(
+    private authService: AuthService,
+    private router: Router,
+  ) { }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+
+    return this.authService.checkAuthentication()
+    .pipe(
+      tap( isAuthenticated => console.log('Authenticated:', isAuthenticated ) ),
+      tap (isAuthenticated => {
+        if ( isAuthenticated ) {
+          this.router.navigate(['./'])
+        }
+      }),
+      map( isAuthenticated => !isAuthenticated)
+    )
+  }
+
+  canMatch(route: Route, segments: UrlSegment[]): boolean | Observable<boolean> {
+    // console.log('canMatch');
+    // console.log({ route, segments });
+
+     return this.checkAuthStatus();
+  }
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): boolean | Observable<boolean> {
+    // console.log('canActivate');
+    // console.log({ route, state })
+
+     return this.checkAuthStatus();
+  }
+}
