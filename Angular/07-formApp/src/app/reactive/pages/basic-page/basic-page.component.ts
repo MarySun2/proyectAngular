19d2db1ed58dd50d77786906a62143f43dd1cf7c
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-21 02:24:15 +0100

    1ª pasos formulario reactivos

--- Angular/07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
+++ Angular/07-formApp/src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,9 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   templateUrl: './basic-page.component.html',
   styles: ``
 })
-export class BasicPageComponent {
+export class BasicPageComponent implements OnInit {
 
+  // Create a form group with FormControl
+  // public myFormGroup = new FormGroup({
+  //   name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   inStorage: new FormControl(0),
+  //   });
+
+  //otra forma de realizar el form group
+  public myForm!: FormGroup; // Declara la variable sin inicializar
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    // Inicializa el formulario en ngOnInit
+    this.myForm = this.fb.group({
+      name: [''],
+      price: [0],
+      inStorage: [0],
+    });
+  }
+
+    onSave():void {
+      console.log(this.myForm.value);
+    }
 }
+
--- Angular/07-formApp/src/app/reactive/reactive.module.ts
+++ Angular/07-formApp/src/app/reactive/reactive.module.ts
@@ -5,6 +5,8 @@ import { BasicPageComponent } from './pages/basic-page/basic-page.component';
 import { DynamicPageComponent } from './pages/dynamic-page/dynamic-page.component';
 import { SwitchesPageComponent } from './pages/switches-page/switches-page.component';
 
+import { ReactiveFormsModule } from '@angular/forms';
+
 
 
 @NgModule({
@@ -15,7 +17,8 @@ import { SwitchesPageComponent } from './pages/switches-page/switches-page.compo
   ],
   imports: [
     CommonModule,
-    ReactiveRoutingModule
+    ReactiveRoutingModule,
+    ReactiveFormsModule
   ]
 })
 export class ReactiveModule { }
