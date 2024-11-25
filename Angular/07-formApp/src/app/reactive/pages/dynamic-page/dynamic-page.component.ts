2bd0fedb1bb96f8e4236e96d675114f623a02c5a
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-25 20:50:56 +0100

    Formularios Dinamicos

--- Angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ Angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -1,9 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './dynamic-page.component.html',
   styles: ``
 })
-export class DynamicPageComponent {
+export class DynamicPageComponent implements OnInit {
+
+  public myForm!: FormGroup; // Declara la variable sin inicializar
+
+  constructor(private fb: FormBuilder ) {}
+
+  get favoriteGames() {
+    return this.myForm.get('favoriteGames') as FormArray;
+  }
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      name: ['', [Validators.required, Validators.minLength(3) ] ],
+      favoriteGames: this.fb.array ([
+         ['Metal Gear', Validators.required ],
+         ['Death Stranding', Validators.required ],
+       ])
+    });
+  }
+
+  onSumit(): void {
+
+    if ( this.myForm.invalid ) {
+      this.myForm.markAllAsTouched();
+      return;  // Form is invalid, stop processing.
+    }
+    console.log(this.myForm.value);
+    this.myForm.reset();  // Reset form.
+  }
 
 }
