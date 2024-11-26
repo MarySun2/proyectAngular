ac054bb2ccd298f8352b94ddeddbad055028698d
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-26 20:51:37 +0100

    Validacion Dynamica

--- Angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
+++ Angular/07-formApp/src/app/reactive/pages/dynamic-page/dynamic-page.component.ts
@@ -24,6 +24,34 @@ export class DynamicPageComponent implements OnInit {
     });
   }
 
+  isValidField ( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+     && this.myForm.controls[field].touched;
+  }
+
+  isValidFieldInArray(formArray: FormArray, index: number) {
+    return formArray.controls[index].errors
+     && formArray.controls[index].touched;
+    }
+
+  getFieldError( field: string): string | null {
+
+    if ( !this.myForm.controls[field] ) return null;
+
+    const errors = this.myForm.controls[field].errors || {};
+
+      for (const key of Object.keys(errors) ) {
+        switch ( key ) {
+          case 'required':
+            return 'Este campo es requerido';
+
+            case 'minlength':
+              return `Minimo ${ errors['minlength'].requiredLength } caracteres.`;
+        }
+      }
+      return null;
+    }
+
   onSumit(): void {
 
     if ( this.myForm.invalid ) {
