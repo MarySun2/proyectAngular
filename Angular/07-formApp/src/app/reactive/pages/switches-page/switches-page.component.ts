22aaceaeadabf7b46c7ea2026481265957000348
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-27 19:26:58 +0100

    switches

--- Angular/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
+++ Angular/07-formApp/src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
   styles: ``
 })
-export class SwitchesPageComponent {
+export class SwitchesPageComponent implements OnInit {
 
+  // public myform: FormGroup =this.fb.group({
+  //   genter:['M', Validators.required],
+  //   wanNotification: [true, Validators.required],
+  //   termsAndConditions: [false, Validators.requiredTrue],
+  // });
+
+  public myForm!: FormGroup;
+
+  public person = {
+    gender: 'F',
+    wanNotification: false,
+  }
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+      gender:['M', Validators.required],
+      wanNotification: [true, Validators.required],
+      termsAndConditions: [false, Validators.requiredTrue],
+    });
+
+    this.myForm.reset( this.person )
+  }
+
+  isValidField ( field: string ): boolean | null {
+    return this.myForm.controls[field].errors
+     && this.myForm.controls[field].touched;
+  }
+
+  onSave() {
+
+    if ( this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    const {termsAndConditions, ...newPerson} = this.myForm.value
+    this.person = newPerson;
+    console.log(this.myForm.value);
+    console.log(this.person);
+
+
+  }
 }
