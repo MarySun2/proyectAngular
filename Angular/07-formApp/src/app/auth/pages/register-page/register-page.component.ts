bd913e8e2211494c3b85ae41bc4be0d9567324f6
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-27 20:25:58 +0100

    Form Registro

--- Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './register-page.component.html',
   styles: ``
 })
-export class RegisterPageComponent {
+export class RegisterPageComponent implements OnInit {
 
+  // public myForm: FormGroup = this.fb.group({
+  //   name:['', [Validators.required]],
+  //   email:['', [Validators.required]],
+  //   username:['', [Validators.required]],
+  //   password:['', [Validators.required, Validators.minLength(6)]],
+  //   confirmPassword:['', [Validators.required, Validators.minLength(6)]]
+  // })
+
+  public myForm!: FormGroup; // Declara la variable sin inicializar
+
+  constructor( private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.myForm = this.fb.group({
+       name:['', [Validators.required]],
+       email:['', [Validators.required]],
+       username:['', [Validators.required]],
+       password:['', [Validators.required, Validators.minLength(6)]],
+       confirmPassword:['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  isValidField( field: string ){
+    //TODO OBTENER VALIDACION DESDE UN SERVICIO
+  }
+
+  onSumit() {
+    this.myForm.markAllAsTouched();
+  }
 }
