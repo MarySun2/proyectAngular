6070706eb439cb373dd1aabae2dfeb908ac3ca18
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-27 21:55:08 +0100

    validator Functions

--- Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -22,9 +23,9 @@ export class RegisterPageComponent implements OnInit {
     this.myForm = this.fb.group({
        name:['', [Validators.required]],
        email:['', [Validators.required]],
-       username:['', [Validators.required]],
-       password:['', [Validators.required, Validators.minLength(6)]],
-       confirmPassword:['', [Validators.required, Validators.minLength(6)]]
+       username:['', [Validators.required, cantBeStrider ]],
+       password:['', [Validators.required, Validators.minLength(6) ]],
+       confirmPassword:['', [Validators.required, Validators.minLength(6) ]]
     });
   }
 
