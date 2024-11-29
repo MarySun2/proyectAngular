8c8860360075bc8fbd7c224eea2d116411a427e6
Mary <marisol.pereira.goncalves@gmail.com>
2024-11-29 13:46:33 +0100

    validador de correo

--- Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
+++ Angular/07-formApp/src/app/auth/pages/register-page/register-page.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 import { ValidatorsService } from '../../../shared/service/validators.service';
+import { EmailValidatorService } from '../../../shared/validators/email-validator.service';
 
 @Component({
   templateUrl: './register-page.component.html',
@@ -21,16 +22,18 @@ export class RegisterPageComponent implements OnInit {
 
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidatorService
   ) {}
 
   ngOnInit(): void {
     this.myForm = this.fb.group({
        name:['', [Validators.required, Validators.pattern (this.validatorsService.firstNameAndLastnamePattern)]],
-       email:['', [Validators.required, Validators.pattern (this.validatorsService.emailPattern) ]],
-       username:['', [Validators.required, this.validatorsService.cantBeStrider ]],
-       password:['', [Validators.required, Validators.minLength(6) ]],
-       confirmPassword:['', [Validators.required, Validators.minLength(6) ]]
+      //  email:['', [Validators.required, Validators.pattern (this.validatorsService.emailPattern)], [new EmailValidatorService() ]],
+      email:['', [Validators.required, Validators.pattern (this.validatorsService.emailPattern)], [this.emailValidator]],
+      password:['', [Validators.required, Validators.minLength(6) ]],
+      username:['', [Validators.required, this.validatorsService.cantBeStrider ]],
+      confirmPassword:['', [Validators.required, Validators.minLength(6) ]]
     });
   }
 
