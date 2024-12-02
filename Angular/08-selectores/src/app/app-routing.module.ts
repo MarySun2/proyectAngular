815b91065e1c0bc270a662d3fa264583023a5c1b
Mary <marisol.pereira.goncalves@gmail.com>
2024-12-02 17:24:30 +0100

    Estructura de direcctorios

--- Angular/08-selectores/src/app/app-routing.module.ts
+++ Angular/08-selectores/src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'selector',
+    //loadChildren: lazyLoad
+  },
+
+  {
+    path: '**',
+    redirectTo: 'selector',
+    pathMatch: 'full'  // Si no encuentra la ruta, redirige a la ruta por defecto (selector)
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
