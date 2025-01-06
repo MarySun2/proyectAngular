5382d060774856c828d65662cc6060551ed49018
Mary <marisol.pereira.goncalves@gmail.com>
2025-01-06 21:49:42 +0100

    Intallar mapbox y mostrarlo en pantalla completa

--- Angular/10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ Angular/10-mapsApp/src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,23 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl';
+
+mapboxgl.accessToken = 'pk.eyJ1IjoibWFyeTI1NSIsImEiOiJjbTVsZnJuNW4xZ3JjMm5zZDNicXQzbHlwIn0.hBmmRL5FAIiaJr0kdQ1fzQ';
 
 @Component({
   templateUrl: './full-screen-page.component.html',
   styleUrl: './full-screen-page.component.css'
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
 
 }
