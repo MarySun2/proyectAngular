6ff0948e35a6b0621f38fff69b45c6206e4ad409
Mary <marisol.pereira.goncalves@gmail.com>
2025-02-10 21:36:24 +0100

    Guardar y leer localStorage

--- Angular/10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
+++ Angular/10-mapsApp/src/app/maps/pages/markers-page/markers-page.component.ts
@@ -6,15 +6,22 @@ interface MarkerAndColor {
   marker: Marker;
 }
 
+interface PlainMarker {
+  color: string;
+  lngLat: number[]
+}
+
+
 @Component({
   templateUrl: './markers-page.component.html',
-  styleUrl: './markers-page.component.css'
+  styleUrls: ['./markers-page.component.css']
 })
 export class MarkersPageComponent {
 
   @ViewChild('map') divMap?: ElementRef;
 
-  public markers: MarkerAndColor [] = []; // Array of Mark
+  public markers: MarkerAndColor[] = [];
+
 
   public map?: Map;
   public currentLngLat: LngLat = new LngLat(-74.10380784179445, 4.651165392795477);
@@ -28,58 +35,90 @@ export class MarkersPageComponent {
       container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: this.currentLngLat,
-      zoom: 13, // starting zoom
+      zoom: 13
     });
 
-    // const markerHtml = document.createElement('div'); //Marcador Personalizado
-    // markerHtml.innerHTML='Fernando Herrera'
+    this.readFromLocalStorage();
 
-  //   const marker =new Marker({
-  //     color: 'green',
-       // element: markerHtml Marcador personalizado
-  // })
-  //   .setLngLat( this.currentLngLat )
-  //   .addTo( this.map );
+    // const markerHtml = document.createElement('div');
+    // markerHtml.innerHTML = 'Fernando Herrera'
+
+    // const marker = new Marker({
+    //   // color: 'red',
+    //   element: markerHtml
+    // })
+    //   .setLngLat( this.currentLngLat )
+    //   .addTo( this.map );
+
   }
-
-  //Metodos
 
   createMarker() {
+    if ( !this.map ) return;
 
-    if (!this.map ) return;
+    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16));
+    const lngLat = this.map.getCenter();
 
-    const color = '#xxxxxx'.replace(/x/g, y=>(Math.random()*16|0).toString(16)); // Genera un hexadecimal
-    const LngLat = this.map.getCenter();
-
-    this.addMarker(LngLat, color);
+    this.addMarker( lngLat, color );
   }
 
 
-  addMarker(lngLat: LngLat, color: string = 'green') {
-    if (!this.map ) return;
+  addMarker( lngLat: LngLat, color: string ) {
+    if ( !this.map ) return;
 
-    // Creas el marcador
-    const marker = new Marker ({
+    const marker = new Marker({
       color: color,
       draggable: true
     })
-    .setLngLat( lngLat)
-    .addTo( this.map );
+      .setLngLat( lngLat )
+      .addTo( this.map );
 
-    this.markers.push( {
-      color, marker, });
+    this.markers.push({ color, marker, });
+    this.saveToLocalStorage();
+
+    marker.on('dragend', () => this.saveToLocalStorage() );
+
+    // dragend
   }
 
   deleteMarker( index: number ) {
     this.markers[index].marker.remove();
-    this.markers.splice(index, 1);
+    this.markers.splice( index, 1 );
   }
 
-  flyTo (marker: Marker) {
+  flyTo( marker: Marker ) {
+
     this.map?.flyTo({
       zoom: 14,
-      center: marker.getLngLat(),
+      center: marker.getLngLat()
     });
+
   }
+
+
+  saveToLocalStorage() {
+    const plainMarkers: PlainMarker[] = this.markers.map( ({ color, marker }) => {
+      return {
+        color,
+        lngLat: marker.getLngLat().toArray()
+      }
+    });
+
+    localStorage.setItem('plainMarkers', JSON.stringify( plainMarkers ));
+
+  }
+
+  readFromLocalStorage() {
+    const plainMarkersString = localStorage.getItem('plainMarkers') ?? '[]';
+    const plainMarkers: PlainMarker[] = JSON.parse( plainMarkersString ); //! OJO!
+
+    plainMarkers.forEach( ({ color, lngLat }) => {
+      const [ lng, lat ] = lngLat;
+      const coords = new LngLat( lng, lat );
+
+      this.addMarker( coords, color );
+    })
+
+  }
+
 
 }
