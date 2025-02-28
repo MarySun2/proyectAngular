32764ecdf753dd2082ddfadda0b273808f15dc69
Mary <marisol.pereira.goncalves@gmail.com>
2025-02-28 21:44:27 +0100

    dashboard + tarea

--- Angular/12-gifs-app1/src/app/app.routes.ts
+++ Angular/12-gifs-app1/src/app/app.routes.ts
@@ -1,3 +1,36 @@
 import { Routes } from '@angular/router';
+import { Component } from '@angular/core';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+  {
+    path: 'dashboard',
+
+    //Una forma de hacerlo
+    // loadComponent: () => import ('./gifs/pages/dashboard-page/dashboard-page.component').then (
+    //   (c) => c.DashboardPageComponent
+    // ),
+    //Segunda forma de hacerlo
+    loadComponent: () =>
+       import ('./gifs/pages/dashboard-page/dashboard-page.component'),
+  },
+
+  {
+    path: 'trending',
+    loadComponent: () =>
+       import ('./gifs/pages/trending-page/trending-page.component'),
+  },
+
+
+  {
+    path: 'search',
+    loadComponent: () =>
+       import ('./gifs/pages/search-page/search-page.component'),
+  },
+
+  {
+    path: '**',
+    redirectTo: 'dashboard',
+  },
+];
---
+++ Angular/12-gifs-app1/src/app/gifs/pages/dashboard-page/dashboard-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './dashboard-page.component.html',
+  styles: ``
+})
+export default class DashboardPageComponent {
+
+}
