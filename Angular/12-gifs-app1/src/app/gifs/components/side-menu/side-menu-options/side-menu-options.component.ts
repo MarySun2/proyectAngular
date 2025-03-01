c053daf0cd82d2e25ab880f584d514702444fbde
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-01 13:21:31 +0100

    side-menu

---
+++ Angular/12-gifs-app1/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'gifs-side-menu-options',
+  standalone: true,
+  imports: [],
+  templateUrl: './side-menu-options.component.html',
+  styles: ``
+})
+export class SideMenuOptionsComponent {
+
+}
