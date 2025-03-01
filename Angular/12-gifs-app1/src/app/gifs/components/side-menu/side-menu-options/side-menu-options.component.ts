e94f0aa2b80dc573ffeacd99e13b651662d25dfd
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-01 14:30:25 +0100

    RouterLinks

--- Angular/12-gifs-app1/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
+++ Angular/12-gifs-app1/src/app/gifs/components/side-menu/side-menu-options/side-menu-options.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+interface MenuOptions {
+  label: string;
+  subLabel: string;
+  route: string;
+  icon: string;
+}
 
 @Component({
   selector: 'gifs-side-menu-options',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './side-menu-options.component.html',
   styles: ``
 })
 export class SideMenuOptionsComponent {
 
+  menuOptions: MenuOptions[] = [
+    {
+      icon: 'fa-solid fa-chart-line',
+      label: 'Trending',
+      subLabel: 'Gifs Populates',
+      route: '/dashboard/trending',
+    },
+
+    {
+      icon: 'fa-solid fa-magnifying-glass',
+      label: 'Buscador',
+      subLabel: 'Buscar gifs',
+      route: '/dashboard/search',
+    },
+  ];
 }
