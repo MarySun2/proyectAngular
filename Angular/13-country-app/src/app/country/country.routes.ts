fbfbff9292459c58c11aa1ba4cdbfd4350d76a9f
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-21 17:44:35 +0100

    daisyiu y footer

--- Angular/13-country-app/src/app/country/country.routes.ts
+++ Angular/13-country-app/src/app/country/country.routes.ts
@@ -1,22 +1,24 @@
 import { Routes } from '@angular/router';
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
+import { CountryLayoutComponent } from './layouts/countryLayout/countryLayout.component';
 
 
 export const countryRoutes: Routes = [
   {
     path: '',
-    component: ByCapitalPageComponent,
+    component: CountryLayoutComponent,
+    children: [
+      {
+        path: 'by-capital', // Tambien podria aplicar una ruta basia
+        component: ByCapitalPageComponent,
+      },
+
+      {
+        path: '**',
+        redirectTo: 'by-capital',
+      },
+    ],
   },
-
-  //  {
-  //    path: 'countries',
-  //    loadChildren: () => import('').then(m => m.CountriesModule),
-  //  },
-
-  // {
-  //   path: '**',
-  //   redirectTo: '',
-  // },
 ];
 
 export default countryRoutes;
