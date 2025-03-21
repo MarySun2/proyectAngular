bde6e0a1436f8ec5e2fb1945740d8d1b01c0b7ee
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-21 18:57:39 +0100

    input tablas

--- Angular/13-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
+++ Angular/13-country-app/src/app/country/pages/by-capital-page/by-capital-page.component.ts
@@ -7,4 +7,9 @@ import {Component } from '@angular/core';
   templateUrl: './by-capital-page.component.html',
 
 })
-export class ByCapitalPageComponent { }
+export class ByCapitalPageComponent {
+
+  onSearch(value: string) {
+    console.log({value})
+  }
+}
