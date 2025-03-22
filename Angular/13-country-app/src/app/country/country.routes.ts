b6875ccc81843a50046fd6ea1a4417f18108d46a
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-22 11:54:56 +0100

    Guia de estulos y nuevos componentes

--- Angular/13-country-app/src/app/country/country.routes.ts
+++ Angular/13-country-app/src/app/country/country.routes.ts
@@ -1,6 +1,11 @@
 import { Routes } from '@angular/router';
+
 import { ByCapitalPageComponent } from './pages/by-capital-page/by-capital-page.component';
 import { CountryLayoutComponent } from './layouts/countryLayout/countryLayout.component';
+import { ByCountryPageComponent } from './pages/by country-page/by country-page.component';
+import { ByRegionPageComponent } from './pages/by region-page/by region-page.component';
+import { CountryPageComponent } from './pages/country-page/country-page.component';
+
 
 
 export const countryRoutes: Routes = [
@@ -11,6 +16,22 @@ export const countryRoutes: Routes = [
       {
         path: 'by-capital', // Tambien podria aplicar una ruta basia
         component: ByCapitalPageComponent,
+      },
+
+      {
+        path: 'by-country', // Tambien podria aplicar una ruta basia
+        component: ByCountryPageComponent,
+      },
+
+      {
+        path: 'by-region', // Tambien podria aplicar una ruta basia
+        component: ByRegionPageComponent,
+      },
+
+      //Ruta Dinamica
+      {
+        path: 'by/:code',
+        component: CountryPageComponent,
       },
 
       {
