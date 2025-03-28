ec3c577515c86fdb44c3670c7aa94853686c77ca
Mary <marisol.pereira.goncalves@gmail.com>
2025-03-28 19:41:39 +0100

    info pais

--- Angular/13-country-app/src/app/country/pages/country-page/country-page.component.ts
+++ Angular/13-country-app/src/app/country/pages/country-page/country-page.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CountryService } from '../../services/country.service';
+import { rxResource } from '@angular/core/rxjs-interop';
 
 @Component({
   selector: 'country-page',
@@ -6,4 +9,16 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './country-page.component.html',
 })
-export class CountryPageComponent { }
+export class CountryPageComponent {
+
+  countryCode = inject (ActivatedRoute).snapshot.params['code'];
+  countryService = inject (CountryService);
+
+  countryResource = rxResource({
+    request: () => ({code: this.countryCode}),
+    loader: ({ request }) => {
+      return this.countryService.searchCountryByAlphaCode(request.code)
+    },
+
+  });
+}
