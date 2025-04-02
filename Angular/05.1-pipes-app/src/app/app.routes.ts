3f7c6eef336bb84110d26da879a5ca005a93deec
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-02 21:53:18 +0200

    inicio del proyecto 2

--- Angular/05.1-pipes-app/src/app/app.routes.ts
+++ Angular/05.1-pipes-app/src/app/app.routes.ts
@@ -1,3 +1,34 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+
+{
+  path: 'basic',
+  title: 'Pipes Basicos',
+  loadComponent: ()=> import('./pages/basic-page/basic-page.component')
+},
+
+{
+  path: 'numbers',
+  title: 'Numbers Pipe',
+  loadComponent: ()=> import('./pages/numbers-page/numbers-page.component')
+},
+
+{
+  path: 'uncommon',
+  title: 'Pipes Uncommon',
+  loadComponent: ()=> import('./pages//uncommon-page/uncommon-page.component')
+},
+
+{
+  path: 'custom',
+  title: 'Pipes Custom',
+  loadComponent: ()=> import('./pages/custom-page/custom-page.component')
+},
+
+{
+  path: '**',
+  redirectTo: 'basic',
+},
+
+];
