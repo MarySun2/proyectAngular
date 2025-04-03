aa4318b99ea6ffd5fa5511bdcda6ec50cbe88380
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-03 21:14:45 +0200

    i18nSelect

--- Angular/05.1-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ Angular/05.1-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
+import { I18nSelectPipe } from '@angular/common';
+
+// se puede hacer esto creando un archivo json y creando una interface
+const client1 = {
+  name:'Fernando',
+  gender: 'male',
+  age: 29,
+  address: 'Ottawa, Canada',
+  occupation: 'Developer',
+};
+
+const client2 = {
+  name:'Melissa',
+  gender: 'female',
+  age: 36,
+  address: 'London, UK',
+  occupation: 'UX Designer',
+};
 
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [CardComponent],
+  imports: [CardComponent, I18nSelectPipe],
   templateUrl: './uncommon-page.component.html',
 })
-export default class UncommonPageComponent { }
+export default class UncommonPageComponent {
+
+
+  client = signal (client1);
+
+   //i18n Select
+   invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla',
+   };
+
+
+  changeClient() {
+    if (this.client() === client1) {
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+}
