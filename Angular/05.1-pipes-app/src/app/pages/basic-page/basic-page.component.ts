f4eb89f0863140e27542f730b075db50f1830882
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-03 20:29:41 +0200

    Cambiar el ideoma y local dinamicamente

--- Angular/05.1-pipes-app/src/app/pages/basic-page/basic-page.component.ts
+++ Angular/05.1-pipes-app/src/app/pages/basic-page/basic-page.component.ts
@@ -1,5 +1,6 @@
 import { DatePipe, LowerCasePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
-import { Component, effect, signal } from '@angular/core';
+import { Component, effect, inject, LOCALE_ID, signal } from '@angular/core';
+import { AvailableLocale, LocaleService } from '../../services/locale.service';
 
 @Component({
   selector: 'app-basic-page',
@@ -8,6 +9,10 @@ import { Component, effect, signal } from '@angular/core';
   templateUrl: './basic-page.component.html',
 })
 export default class BasicPageComponent {
+
+  localService = inject(LocaleService);
+  currentLocale= signal (inject(LOCALE_ID));
+
 
   nameLower= signal ('fernando');
   nameUpper= signal ('FERNANDO');
@@ -25,4 +30,9 @@ export default class BasicPageComponent {
     clearInterval(interval);
   });
 });
+
+changeLocale(locale: AvailableLocale) {
+  console.log({locale});
+  this.localService.changeLocale(locale);
+}
 }
