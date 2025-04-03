7a635ee7752175210efc5e3626253c033d65eb59
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-03 21:48:27 +0200

    I18nPlural

--- Angular/05.1-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
+++ Angular/05.1-pipes-app/src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nSelectPipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe } from '@angular/common';
 
 // se puede hacer esto creando un archivo json y creando una interface
 const client1 = {
@@ -22,7 +22,7 @@ const client2 = {
 @Component({
   selector: 'app-uncommon-page',
   standalone: true,
-  imports: [CardComponent, I18nSelectPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -43,5 +43,27 @@ export default class UncommonPageComponent {
       return;
     }
     this.client.set(client1);
+  }
+
+  //I18n Plural
+  clientsMap = signal ({
+    '=0' : 'no tenemos ningun cliente esperando',
+    '=1' : 'tenemos 1 cliente esperando',
+    '=2' : 'tenemos 2 esperando',
+    'other' : 'tenemos # clientes esperando',
+  });
+
+  clients = signal ([
+    'Maria',
+    'Pedro',
+    'Fernando',
+    'Hernando',
+    'Eduardo',
+    'Melisa',
+    'Nataliad',
+  ]);
+
+  deleteClient() {
+    this.clients.update((prev) => prev.slice(1));
   }
 }
