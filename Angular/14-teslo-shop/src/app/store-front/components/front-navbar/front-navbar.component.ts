0c1980f47d7e4c30398bf5c5449f4af16d8e289d
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-10 20:39:08 +0200

    rutas

---
+++ Angular/14-teslo-shop/src/app/store-front/components/front-navbar/front-navbar.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'front-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './front-navbar.component.html',
+})
+export class FrontNavbarComponent { }
