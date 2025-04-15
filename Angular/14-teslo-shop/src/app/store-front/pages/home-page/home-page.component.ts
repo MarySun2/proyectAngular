6eceab3df00287f3279f36e142629ab0c0e33ca2
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-15 14:23:04 +0200

    Creacion del Backen punto 15

--- Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { ProductCardComponent } from '../../components/product-card/product-card.component';
+import { ProductCardComponent } from '@store-front/components/product-card/product-card.component';
+//import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
 @Component({
   selector: 'app-home-page',
