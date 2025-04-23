76afad292fb5f0c3a3e75933346f7e61343dfce0
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-23 15:29:27 +0200

    paginacion funcionalidad

--- Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -1,13 +1,16 @@
 import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@store-front/components/product-card/product-card.component';
 //import { ProductCardComponent } from '../../components/product-card/product-card.component';
-import { ProductsService } from '../../../products/services/products.service';
+
 import { rxResource } from '@angular/core/rxjs-interop';
+import { ProductsService } from '@products/services/products.service';
+import { PaginationComponent } from '@products/shared/components/pagination/pagination.component';
+
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [ProductCardComponent],
+  imports: [ProductCardComponent, PaginationComponent],
   templateUrl: './home-page.component.html',
 })
 export class HomePageComponent {
