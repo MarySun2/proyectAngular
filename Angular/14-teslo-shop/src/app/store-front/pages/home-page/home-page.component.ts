3bf808981ce8fb34a82454e6f8d7c960a65f5ccf
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-23 15:46:12 +0200

    paginacion RxResource

--- Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
+++ Angular/14-teslo-shop/src/app/store-front/pages/home-page/home-page.component.ts
@@ -2,9 +2,11 @@ import { Component, inject } from '@angular/core';
 import { ProductCardComponent } from '@store-front/components/product-card/product-card.component';
 //import { ProductCardComponent } from '../../components/product-card/product-card.component';
 
-import { rxResource } from '@angular/core/rxjs-interop';
+import { rxResource, toSignal } from '@angular/core/rxjs-interop';
 import { ProductsService } from '@products/services/products.service';
 import { PaginationComponent } from '@products/shared/components/pagination/pagination.component';
+import { ActivatedRoute } from '@angular/router';
+import { map } from 'rxjs';
 
 
 @Component({
@@ -16,10 +18,24 @@ import { PaginationComponent } from '@products/shared/components/pagination/pagi
 export class HomePageComponent {
   productsService = inject(ProductsService);
 
+  activatedRoute = inject(ActivatedRoute);
+
+  CurrentPage = toSignal (
+    this.activatedRoute.queryParamMap.pipe(
+      map( params => (params.get('page') ? + params.get('page')! : 1)),
+      map( page => (isNaN(page) ? 1 : page) )
+    ),
+    {
+      initialValue: 1,
+    }
+  )
+
   productsResource = rxResource({
-    request: () => ({}),
+    request: () => ({ page: this.CurrentPage() -1 }),
     loader: ({ request }) => {
-      return this.productsService.getProducts({});
+      return this.productsService.getProducts({
+        offset: request.page * 9,
+      });
     },
   });
  }
