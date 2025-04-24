1562e3b1154bbdc1d731e10ee0c25afe6d6b1937
Mary <marisol.pereira.goncalves@gmail.com>
2025-04-24 12:55:15 +0200

    cache producto

--- Angular/14-teslo-shop/src/app/products/services/products.service.ts
+++ Angular/14-teslo-shop/src/app/products/services/products.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
 import { Product, ProductsResponse } from '@products/interfaces/product.interface';
-import { Observable, of, tap } from 'rxjs';
+import { delay, Observable, of, tap } from 'rxjs';
 import { environment } from 'src/environments/environment';
 
 const baseUrl = environment.baseUrl;
@@ -17,6 +17,7 @@ export class ProductsService {
   private http = inject(HttpClient);
 
   private productsCache = new Map<string,ProductsResponse>();
+  private productCache = new Map<string, Product>();
 
   getProducts(options: Options): Observable<ProductsResponse> {
     const { limit = 9, offset = 0, gender = '' } = options;
@@ -41,6 +42,13 @@ export class ProductsService {
   }
 
   getProductByIdSlug(idSlug: string): Observable<Product> {
-    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`);
+    if (this.productsCache.has(idSlug)) {
+      return of (this.productCache.get(idSlug)!);
+    }
+    return this.http.get<Product>(`${baseUrl}/products/${idSlug}`)
+    .pipe(
+      // delay(2000),
+      tap((product)=> this.productCache.set(idSlug, product))
+    );
   }
 }
