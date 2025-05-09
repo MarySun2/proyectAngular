32c4e2af9f3048c04cb3e9dcc7a76d13c7510289
Mary <marisol.pereira.goncalves@gmail.com>
2025-05-09 18:44:11 +0200

    Rutas, auth, login, registro

--- Angular/14-teslo-shop/src/app/app.routes.ts
+++ Angular/14-teslo-shop/src/app/app.routes.ts
@@ -1,6 +1,13 @@
 import { Routes } from '@angular/router';
 
 export const routes: Routes = [
+
+  {
+    path: 'auth',
+    loadChildren: () => import('./auth/auth.routes'),
+    //TO DO GUARDS
+  },
+
   {
     path: '',
     loadChildren: () => import('./store-front/store-front.routes'),
