fe9be1a5a35cec42713dd79bc9947530196dd0f6
Mary <marisol.pereira.goncalves@gmail.com>
2025-05-12 21:16:08 +0200

    control de autenticacion

--- Angular/14-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
+++ Angular/14-teslo-shop/src/app/auth/pages/login-page/login-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, inject, signal } from '@angular/core';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RouterLink } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
 
 @Component({
   selector: 'app-login-page',
@@ -13,6 +14,8 @@ export class LoginPageComponent {
   fb = inject (FormBuilder)
   hasError = signal(false)
   isPosting = signal(false)
+
+  authService = inject (AuthService);
 
   loginForm = this.fb.group({
     email: ['', [ Validators. required, Validators.email ]],
@@ -30,6 +33,8 @@ export class LoginPageComponent {
 
     const {email = '', password = ''} = this.loginForm.value;
 
-    console.log({email, password});
+    this.authService.login(email!, password!). subscribe(resp => {
+    console.log(resp);
+    })
   }
 }
