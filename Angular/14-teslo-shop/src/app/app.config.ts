11cb1531301733d4f08167a0491d9de6ab669e30
Mary <marisol.pereira.goncalves@gmail.com>
2025-06-01 15:44:12 +0200

    para desplegar

--- Angular/14-teslo-shop/src/app/app.config.ts
+++ Angular/14-teslo-shop/src/app/app.config.ts
@@ -1,16 +1,16 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
 
-import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
+//import { loggingInterceptor } from '@shared/interceptors/logging.interceptor';
 import { authInterceptor } from '@auth/interceptors/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideRouter(routes , withHashLocation()), //   =>#/home
     provideHttpClient(
       withFetch(),
       withInterceptors([
