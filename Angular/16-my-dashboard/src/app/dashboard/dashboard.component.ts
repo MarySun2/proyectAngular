d74fbb181c96b067dc04bcaeccd20babfa977a27
Mary <marisol.pereira.goncalves@gmail.com>
2025-06-09 22:24:30 +0200

    Configuracion dasboard y reparar taiwing

--- Angular/16-my-dashboard/src/app/dashboard/dashboard.component.ts
+++ Angular/16-my-dashboard/src/app/dashboard/dashboard.component.ts
@@ -1,11 +1,30 @@
 
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { SidemenuComponent } from '../shared/sidemenu/sidemenu.component';
+import { routes } from '../app.routes';
+
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, SidemenuComponent],
   templateUrl: './dashboard.component.html',
 })
-export default class DashboardComponent { }
+export default class DashboardComponent {
+
+  public MenuItem = routes
+    .map( route => route.children ?? [] )
+    .flat() // Aplana
+    .filter( route => route && route.path )
+    .filter( route => !route.path?.includes(':'));
+
+  constructor() {
+    // const dashboardRoutes = routes
+    // .map( route => route.children ?? [] )
+    // .flat() // Aplana
+    // .filter( route => route && route.path )
+    // .filter( route => !route.path?.includes(':') )
+    // console.log(dashboardRoutes);
+  }
+}
