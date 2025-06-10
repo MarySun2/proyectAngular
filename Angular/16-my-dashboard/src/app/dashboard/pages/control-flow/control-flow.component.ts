cda19dde0a8794aad387fa97acf9913d0a193aa8
Mary <marisol.pereira.goncalves@gmail.com>
2025-06-10 12:39:08 +0200

    swich

--- Angular/16-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
+++ Angular/16-my-dashboard/src/app/dashboard/pages/control-flow/control-flow.component.ts
@@ -1,5 +1,7 @@
 import { Component, signal } from '@angular/core';
 
+type Grade = 'A'| 'B' | 'F';
+
 @Component({
   selector: 'app-control-flow',
   standalone: true,
@@ -7,7 +9,9 @@ import { Component, signal } from '@angular/core';
   templateUrl: './control-flow.component.html',
 })
 export default class ControlFlowComponent {
+  //Propiedad
   public showContent = signal (false);//.asReadonly; // no podria cambiar el valor
+  public grade = signal<Grade>('A');
 
   // Metodo
   public toggleContent () {
