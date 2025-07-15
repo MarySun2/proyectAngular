240b9212e70ba262efbcdb57047dcee1a7f7796b
Mary <marisol.pereira.goncalves@gmail.com>
2025-07-15 21:42:52 +0200

    @if

--- Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,5 @@
-import { Component, signal } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, computed, signal } from '@angular/core';
 
 
 interface Character {
@@ -10,15 +11,24 @@ interface Character {
 @Component({
   selector: 'app-dragonball-page',
   standalone: true,
-  imports: [],
+  imports: [
+            //NgClass
+           ],
   templateUrl: './dragonball-page.component.html',
 })
 export class DragonballPageComponent {
 
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 2, name: 'Vegeta', power: 9000 },
     { id: 3, name: 'Pikoro', power: 3000 },
+    { id: 4, name: 'Yamcha', power: 500 },
   ]);
 
+  //clase computada
+  // powerClasses = computed( () => {
+  //   return{
+  //     'text-danger': true,
+  //   };
+  // });
  }
