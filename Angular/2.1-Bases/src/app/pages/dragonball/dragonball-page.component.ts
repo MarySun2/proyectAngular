7aef1ec10bfeeea3a86e341aeb24651b823a5b56
Mary <marisol.pereira.goncalves@gmail.com>
2025-07-15 21:13:45 +0200

    @fof

--- Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,4 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+
+
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 
 @Component({
   selector: 'app-dragonball-page',
@@ -6,4 +13,12 @@ import { Component } from '@angular/core';
   imports: [],
   templateUrl: './dragonball-page.component.html',
 })
-export class DragonballPageComponent { }
+export class DragonballPageComponent {
+
+  characters = signal<Character[]>([
+    { id: 1, name: 'Goku', power: 9001 },
+    { id: 2, name: 'Vegeta', power: 8000 },
+    { id: 3, name: 'Pikoro', power: 3000 },
+  ]);
+
+ }
