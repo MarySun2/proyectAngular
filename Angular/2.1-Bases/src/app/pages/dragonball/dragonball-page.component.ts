7b3386a3884337360446f86aec60a84fa444ccaf
Mary <marisol.pereira.goncalves@gmail.com>
2025-07-15 20:52:51 +0200

    Tarea

---
+++ Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-dragonball-page',
+  standalone: true,
+  imports: [],
+  templateUrl: './dragonball-page.component.html',
+})
+export class DragonballPageComponent { }
