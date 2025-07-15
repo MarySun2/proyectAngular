c3708d8ec34b7dc69a648d1c79a38ea9a4eee8ef
Mary <marisol.pereira.goncalves@gmail.com>
2025-07-15 22:31:29 +0200

    Tarea

--- Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
+++ Angular/2.1-Bases/src/app/pages/dragonball/dragonball-page.component.ts
@@ -18,6 +18,9 @@ interface Character {
 })
 export class DragonballPageComponent {
 
+  name = signal('Gohan');
+  power = signal(100);
+
   characters = signal<Character[]>([
     { id: 1, name: 'Goku', power: 9001 },
     { id: 2, name: 'Vegeta', power: 9000 },
@@ -31,4 +34,29 @@ export class DragonballPageComponent {
   //     'text-danger': true,
   //   };
   // });
+
+  //Metodo
+  addCharacter() {
+    // console.log(this.name(), this.power() );
+    if (!this.name() || !this.power() || this.power() <= 0) {
+      return;
+    }
+
+    // Agrego el nuevo Caracter
+    const newCharacter: Character = {
+      id: this.characters().length + 1,
+      name: this.name(),
+      power: this.power(),
+    };
+
+    // this.characters().push(newCharacter);
+    this.characters.update((list) => [...list, newCharacter]);
+    this.resetField();
+  }
+
+  // pulgarlo
+  resetField() {
+    this.name.set('');
+    this.power.set(0);
+  }
  }
