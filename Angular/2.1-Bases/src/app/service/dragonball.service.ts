4e90566ff21985d0b9c642bb68f3f4b42e008c65
Mary <marisol.pereira.goncalves@gmail.com>
2025-07-17 14:17:01 +0200

    linkedSignal local storage

--- Angular/2.1-Bases/src/app/service/dragonball.service.ts
+++ Angular/2.1-Bases/src/app/service/dragonball.service.ts
@@ -1,15 +1,25 @@
 import { effect, Injectable, signal } from '@angular/core';
 import { Character } from '../interfaces/character.interface';
 
+
+const loadFromLocalStorage = (): Character[] => {
+  const characters = localStorage.getItem('characters');
+
+  return characters ? JSON.parse(characters) : [];
+
+};
+
 @Injectable({
   providedIn: 'root'
 })
 export class DragonballService {
 
-  characters = signal<Character[]>([
-    { id: 1, name: 'Goku', power: 9001 },
-    { id: 2, name: 'Vegeta', power: 9000 },
-  ]);
+  //characters = signal<Character[]>([
+  //   { id: 1, name: 'Goku', power: 9001 },
+  //   { id: 2, name: 'Vegeta', power: 9000 },
+  // ]);
+
+  characters = signal<Character[]>(loadFromLocalStorage());
 
   addCharacter(character: Character) {
     this.characters.update(
